692926ad5d81f86ca23104bc3cc7863597c485de
Developer <{ID}+{username}@users.noreply.github.com>
2024-07-03 11:03:34 +0530

    SocketCors

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,35 +6,36 @@ exports.signup = async (req, res) => {
 	try {
 		const { fullName, username, email, password } = req.body;
 
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		if (!emailRegex.test(email)) {
-			return res.status(400).json({ error: "Invalid email format" });
-		}
-
+		// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		// if (!emailRegex.test(email)) {
+		// 	return res.status(400).json({ error: "Invalid email format" });
+		// }
+		
 		const existingUser = await User.findOne({ username });
+		console.log("existingUser ",existingUser);
 		if (existingUser) {
 			return res.status(400).json({ error: "Username is already taken" });
 		}
-
+		console.log("registration1");
 		const existingEmail = await User.findOne({ email });
 		if (existingEmail) {
 			return res.status(400).json({ error: "Email is already taken" });
 		}
-
+		console.log("registration2");
 		if (password.length < 6) {
 			return res.status(400).json({ error: "Password must be at least 6 characters long" });
 		}
-
+		console.log("registration3");
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
-
+		
 		const newUser = new User({
 			fullName,
 			username,
 			email,
 			password: hashedPassword,
 		});
-
+			console.log(newUser);
 		if (newUser) {
 			await newUser.save();
 			const token = generateToken(newUser._id);
