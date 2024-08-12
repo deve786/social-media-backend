4166c9183c54869200b20235fdf9830206960367
Developer <{ID}+{username}@users.noreply.github.com>
2024-08-12 19:05:53 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const cloudinary = require("cloudinary").v2;
 const cors = require('cors');
 const connectMongoDB = require('./db/dbConnect');
 const authRoutes = require('./routes/authRoutes');
